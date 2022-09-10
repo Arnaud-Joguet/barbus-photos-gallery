d24de2aa1ac1b10545e875074bd4630c52b59d49
JOGUET-RECCORDON Arnaud <arnaud.joguet@gmail.com>
2022-09-10 12:25:29 +0200

    mentions légales WIP

---
+++ public/mentions_legales.php
@@ -0,0 +1,15 @@
+<?php
+include_once __DIR__."../../public/templates/inc/header.tpl.php";
+?>
+<section class="segment">
+    <h2 class="segment__title">Mentions Légales :</h2>
+    <div>
+        <p>Equipe de sport amateur. </p>
+        <p>L'association régie par la loi 1901 à été crée en janvier 2020.</p>
+        
+    </div>
+
+</section>
+
+<?php
+include_once __DIR__."../../public/templates/inc/footer.tpl.php";
