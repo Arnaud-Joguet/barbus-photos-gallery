49d07b2e8cc2a37d8dc213aa4a0eda0a096f5cb2
JOGUET-RECCORDON Arnaud <arnaud.joguet@gmail.com>
2022-09-10 10:48:56 +0200

    WIP presentation team before foreach

---
+++ public/l_equipe.php
@@ -0,0 +1,63 @@
+<?php
+include_once __DIR__."../../public/templates/inc/header.tpl.php";
+?>
+<section class="segment">
+    <h2 class="segment__title">L'équipe :</h2>
+    <div>
+        <img src="images/img-l-equipe.jpg" alt="photo Team barbu's à la création">
+    </div>
+    <!-- mise en place avant optimisation dans une boucle -->
+    <div class="cards">
+        <article class="card">
+            <div class="card__content">
+                <img class="card__image" src="images/pres-snipe.jpg" alt="Snipe">
+                <h3 class="card__title">Défenseur et Capitaine : #6 Snipe </h3>
+            </div>
+        </article>
+        <article class="card">
+            <div class="card__content">
+                <img class="card__image" src="images/pres-pepere.jpg" alt="Pepere">
+                <h3 class="card__title">Attaquant : #40 Pépère </h3>
+            </div>
+        </article>
+        <article class="card">
+            <div class="card__content">
+                <img class="card__image" src="images/pres-zben.jpg" alt="ZBen">
+                <h3 class="card__title">Attaquant : #16 Z-Ben </h3>
+            </div>
+        </article>
+        <article class="card">
+            <div class="card__content">
+                <img class="card__image" src="images/pres-minot.jpg" alt="Minot">
+                <h3 class="card__title">Attaquant : #26 Minot </h3>
+            </div>
+        </article>
+        <article class="card">
+            <div class="card__content">
+                <img class="card__image" src="images/pres-djo.jpg" alt="Djo">
+                <h3 class="card__title">Défenseur : #12 D'Jo </h3>
+            </div>
+        </article>
+        <article class="card">
+            <div class="card__content">
+                <img class="card__image" src="images/pres-papy.jpg" alt="Papy">
+                <h3 class="card__title">Défenseur : #13 Papy </h3>
+            </div>
+        </article>
+        <article class="card">
+            <div class="card__content">
+                <img class="card__image" src="images/pres-tic.jpg" alt="Tic">
+                <h3 class="card__title">Attaquant : #85 Tic </h3>
+            </div>
+        </article>
+        <article class="card">
+            <div class="card__content">
+                <img class="card__image" src="images/pres-tac.jpg" alt="Tac">
+                <h3 class="card__title">Attaquant : #9 Tac </h3>
+            </div>
+        </article>
+    </div>
+</section>
+
+<?php
+include_once __DIR__."../../public/templates/inc/footer.tpl.php";
